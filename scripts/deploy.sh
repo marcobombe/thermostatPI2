9ba8180d509e46e68696af1812465592663fdb8d
marcobombe <marcobombe@gmail.com>
2019-02-10 18:04:00 +0100

    App skeleton and scripts added.
    file:///home/marcobombe/workspace/thermostatPi2/scripts/deploy.sh
    file:///home/marcobombe/workspace/thermostatPi2/scripts/generate_ui.sh
    file:///home/marcobombe/workspace/thermostatPi2/thermostatPi2.py
    file:///home/marcobombe/workspace/thermostatPi2/ui/mainwindow.ui
    file:///home/marcobombe/workspace/thermostatPi2/README.md

---
+++ scripts/deploy.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+echo DEPLOY
+scp -r ../../thermostatPi2 pi@192.168.1.67:/home/pi/
---
+++ scripts/generate_ui.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+echo GENERATING UI
+pyuic5 ../ui/mainwindow.ui > ../mainwindow_auto.py
