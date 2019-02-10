87d30a7e1f7b4b98b7c3a8e1c31cb4d52fc5df05
marcobombe <marcobombe@gmail.com>
2019-02-10 18:32:32 +0100

    Quit button added (only for debug)
    file:///home/marcobombe/workspace/thermostatPi2/thermostatPi2.py

--- thermostatPi2.py
+++ thermostatPi2.py
@@ -1,29 +1,44 @@
 # always seem to need this
+import os
 import sys
- 
+
 # This gets the Qt stuff
-import PyQt5
+from PyQt5 import QtCore
 from PyQt5.QtWidgets import *
- 
+
 # This is our window from QtCreator
 import mainwindow_auto
- 
+
+
 # create class for our Raspberry Pi GUI
 class MainWindow(QMainWindow, mainwindow_auto.Ui_MainWindow):
- # access variables inside of the UI's file
- def __init__(self):
-  super(self.__class__, self).__init__()
-  self.setupUi(self) # gets defined in the UI file
- 
+    # access variables inside of the UI's file
+
+    # functions for the buttons to call
+    def pressedOnButton(self):
+        print("Application quit.")
+        QtCore.QCoreApplication.instance().quit()
+
+    def __init__(self):
+        super(self.__class__, self).__init__()
+
+        self.setupUi(self)  # gets defined in the UI file
+
+        # Hooks to for buttons
+        self.exitButton.clicked.connect(lambda: self.pressedOnButton())
+
+
 # I feel better having one of these
 def main():
- # a new app instance
- app = QApplication(sys.argv)
- form = MainWindow()
- form.show()
- # without this, the script exits immediately.
- sys.exit(app.exec_())
- 
+    # a new app instance
+    app = QApplication(sys.argv)
+    form = MainWindow()
+    form.show()
+
+    # without this, the script exits immediately.
+    sys.exit(app.exec_())
+
+
 # python bit to figure how who started This
 if __name__ == "__main__":
- main()
+    main()
